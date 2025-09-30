ae26a32a1494ae4dc6ef66092c22da6b4c6a28b5
4r2awy <abdelrahamnshark2025@gmail.com>
2025-10-01 02:35:25 +0300

    App init

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(calc());
+}
+
+class calc extends StatelessWidget {
+  const calc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
